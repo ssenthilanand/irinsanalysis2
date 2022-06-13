a0ba1020455b140e183aae165177dd6e10a9046a
Senthil Anand <ssenthilanand@gmail.com>
2022-06-13 10:52:29 +0530

    Automated circle generation. All sub areas done.

---
+++ data/summary.js
@@ -0,0 +1,107 @@
+﻿const summary = [
+    {
+        "Area": "Software",
+        "Description": "Computing, Algorithms, Software, Databases",
+        "Code": "111",
+        "Faculty": "52",
+        "Category": "E"
+    },
+    {
+        "Area": "Signal Processing",
+        "Description": "Signal Processing, Image Processing, Pattern Recognition",
+        "Code": "105",
+        "Faculty": "46",
+        "Category": "E"
+    },
+    {
+        "Area": "AI",
+        "Description": "Artificial Intelligence, Machine Learning, Data Mining, Big Data Analytics",
+        "Code": "114",
+        "Faculty": "46",
+        "Category": "E"
+    },
+    {
+        "Area": "Automation",
+        "Description": "Sensors, Instrumentation, Control and Automation",
+        "Code": "104",
+        "Faculty": "43",
+        "Category": "E"
+    },
+    {
+        "Area": "Networking",
+        "Description": "Networks and Networking , other than Wireless and Security",
+        "Code": "109",
+        "Faculty": "43",
+        "Category": "E"
+    },
+    {
+        "Area": "Communications",
+        "Description": "Communications, Satellite Communication, Multimedia, RF, Antennas",
+        "Code": "106",
+        "Faculty": "35",
+        "Category": "E"
+    },
+    {
+        "Area": "Electronics",
+        "Description": "Electronics, Power Electronics, Medical Electronics, Mechatronics",
+        "Code": "103",
+        "Faculty": "33",
+        "Category": "E"
+    },
+    {
+        "Area": "Wireless Networks",
+        "Description": "Wireless Networks, including Sensor Networks",
+        "Code": "107",
+        "Faculty": "29",
+        "Category": "E"
+    },
+    {
+        "Area": "Security",
+        "Description": "Cryptography and Network Security",
+        "Code": "108",
+        "Faculty": "27",
+        "Category": "E"
+    },
+    {
+        "Area": "Electrical Power",
+        "Description": "High Voltage/ Power Engg., Power Transmission, Electrical Motors and Machines",
+        "Code": "101",
+        "Faculty": "25",
+        "Category": "E"
+    },
+    {
+        "Area": "Internet",
+        "Description": "The Web, Cloud and Grid Computing",
+        "Code": "112",
+        "Faculty": "24",
+        "Category": "E"
+    },
+    {
+        "Area": "Circuit Design",
+        "Description": "Circuits - Analog and Digital, Chip Design, VLSI",
+        "Code": "102",
+        "Faculty": "19",
+        "Category": "E"
+    },
+    {
+        "Area": "Hardware",
+        "Description": "Computer Hardware and Architecture",
+        "Code": "110",
+        "Faculty": "18",
+        "Category": "E"
+    },
+    {
+        "Area": "IOT",
+        "Description": "Internet of Things, Embedded Systems, Real Time Systems",
+        "Code": "115",
+        "Faculty": "14",
+        "Category": "E"
+    },
+    {
+        "Area": "NLP",
+        "Description": "Natural Language Processing, Text Analytics, Machine Translation",
+        "Code": "113",
+        "Faculty": "12",
+        "Category": "E"
+    }
+]
