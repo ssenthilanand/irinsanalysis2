01a1ac1a79dabcd21ef72aa6db616ae04df12eca
Senthil Anand <ssenthilanand@gmail.com>
2022-07-08 11:29:17 +0530

    Supercluster initial version finished.

---
+++ irinssupercluster/summary.js
@@ -0,0 +1,50 @@
+const summary = [
+    {
+        "Area1": "Networks, the Net",
+        "Area2": "and the Web",
+        "Description": "Networks, Networking, Security, Wireless & Sensor Networks, Web, Cloud and Grid Computing, IoT",
+        "Code": "1",
+        "Faculty": "133",
+        "Category": "E",
+    },
+    {
+        "Area1": "Electronics &",
+        "Area2": "Automation",
+        "Description": "Circuit Design, Chip Design, VLSI, Electronics, Power Electronics, Medical Electronics, Mechatronics, Avionics, Sensors, Instrumentation, Control, Automation",
+        "Code": "2",
+        "Faculty": "85",
+        "Category": "E"
+    },
+    {
+        "Area1": "Communication &",
+        "Area2": "Signal Processing",
+        "Description": "Communication, RF & Microwave, Radiation & Antennas, Signal&Image Processing, Pattern Recognition, Multimedia",
+        "Code": "3",
+        "Faculty": "77",
+        "Category": "E"
+    },
+    {
+        "Area1": "Computers &",
+        "Area2": "Databases",
+        "Description": "Computer Hardware, Architecture, Computer Software, Databases, Computing",
+        "Code": "4",
+        "Faculty": "68",
+        "Category": "E"
+    },
+    {
+        "Area1": "Artificial Intelligence",
+        "Area2": "& Analytics",
+        "Description": "AI, Machine Learning, Data Mining, Big Data Analytics, NLP, Text Analytics, Machine Translation",
+        "Code": "5",
+        "Faculty": "54",
+        "Category": "E"
+    },
+    {
+        "Area1": "High Voltage",
+        "Area2": "/ Power ",
+        "Description": "High Voltage, Power Systems, Motors, Machines",
+        "Code": "6",
+        "Faculty": "24",
+        "Category": "E"
+    },
+]
